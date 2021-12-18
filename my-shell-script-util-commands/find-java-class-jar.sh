c8cf76b3febc1a2f70aefb7b7c40532f262da172
Saeid Noroozi <s.noroozi.cs@gmail.com>
2021-12-18 09:25:05 +0330

    Update find-java-class-jar.sh

--- my-shell-script-util-commands/find-java-class-jar.sh
+++ my-shell-script-util-commands/find-java-class-jar.sh
@@ -1,1 +1,10 @@
+#${1} --> first parameter is path that you want to search on it
+#${2} --> seconf parameter is the class name that you want to search 
+
+
+#using zipinfo 
 for f in $(find "${1}" -name "*.jar"); do zipinfo $f | grep "${2}" && echo $f ; done
+
+
+#using jdk/jre jar
+ for f in $(find ${1} -name *.jar -type f); do ${JAVAM_HOME}/bin/jar --list --file  $f | grep -i ${2} && echo "--->"$f && echo; done;
