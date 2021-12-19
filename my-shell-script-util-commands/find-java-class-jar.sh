d8297e255db7d3cca1283cb900f4dec9d4a35bbc
Saeid Noroozi <s.noroozi.cs@gmail.com>
2021-12-19 12:36:28 +0330

    Update find-java-class-jar.sh

--- my-shell-script-util-commands/find-java-class-jar.sh
+++ my-shell-script-util-commands/find-java-class-jar.sh
@@ -9,4 +9,8 @@ for f in $(find "${1}" -name "*.jar"); do zipinfo $f | grep "${2}" && echo $f ; 
 
 
 #using jdk/jre jar
- for f in $(find ${1} -name *.jar -type f); do ${JAVAM_HOME}/bin/jar --list --file  $f | grep -i ${2} && echo "--->"$f && echo; done;
+for f in $(find ${1} -name *.jar -type f); do ${JAVAM_HOME}/bin/jar --list --file  $f | grep -i ${2} && echo "--->"$f && echo; done;
+
+
+#using unzip
+for f in $(find ${1} -name *.jar -type f); do unzip -l $f | grep -i ${2} && echo "--->"$f && echo; done;
